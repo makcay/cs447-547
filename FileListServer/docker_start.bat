695600948caba7cc8f6a4a82f49853fcb269b165
burak-kara <burak.kara@ozu.edu.tr>
2021-12-26 11:48:38 +0300

    updated readme. added files folder and nano to docker image. fixed .bat file commenting issue. deleted .idea folders added by Intellij.

--- FileListServer/docker_start.bat
+++ FileListServer/docker_start.bat
@@ -1,11 +1,11 @@
 docker build . -t file_server
 docker run -d --rm -w "//app/tc" -p 5000:5000/udp -p 5001:5001/udp --name file_server --cap-add=NET_ADMIN file_server "//bin/bash" ./tc_policy.sh
 
-# following two lines runs server code in two ports 5000 and 5001
-# You can comment them and run server in the docker manually. In this way you, can see server logs
-# You can run server from cli of the docker simply by giving java -classpath ./FileListServer-1.0.0.jar:./lib/* server.FileListServer 5000
+@REM # following two lines runs server code in two ports 5000 and 5001
+@REM # You can comment them and run server in the docker manually. In this way you, can see server logs
+@REM # You can run server from cli of the docker simply by giving java -classpath ./FileListServer-1.0.0.jar:./lib/* server.FileListServer 5000
 docker exec -d -w "//app" -it file_server java -classpath ./FileListServer-1.0.0.jar:./lib/* server.FileListServer 5000
 docker exec -d -w "//app" -it file_server java -classpath ./FileListServer-1.0.0.jar:./lib/* server.FileListServer 5001
 
-# for tc logs
+@REM # for tc logs
 docker logs --follow file_server
